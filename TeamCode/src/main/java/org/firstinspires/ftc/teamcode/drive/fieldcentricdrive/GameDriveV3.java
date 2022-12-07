ad5b4e852fd669c1949023397836abc58cb97ce3
dgirard96789 <david.girard@gmail.com>
2022-12-07 14:18:51 -0800

    added code for speed scaling

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/fieldcentricdrive/GameDriveV3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/fieldcentricdrive/GameDriveV3.java
@@ -40,6 +40,44 @@ public class GameDriveV3 extends LinearOpMode {
     double speedMultiplier = .5;
     double colorvalues[] = {0F, 0F, 0F};
     double shuttlePositionForAutoPickup = 1;
+    private boolean scaleTheDriveInput = true;
+
+
+    /*
+     * This method scales the joystick input so for low joystick values, the
+     * scaled value is less than linear.  This is to make it easier to drive
+     * the robot more precisely at slower speeds.
+     */
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
+    }
+
+
 
 
     @Override
@@ -73,13 +111,27 @@ public class GameDriveV3 extends LinearOpMode {
 
             // Pass in the rotated input + right stick value for rotation
             // Rotation is not part of the rotated input thus must be passed in separately
-            drive.setWeightedDrivePower(
-                    new Pose2d(
-                            input.getX(),
-                            input.getY(),
-                            -gamepad1.right_stick_x *0.7
-                    )
-            );
+//this section was the original unscaled power setting
+if (scaleTheDriveInput) {
+    drive.setWeightedDrivePower(
+            new Pose2d(
+                    scaleInput(input.getX()),
+                    scaleInput(input.getY()),
+                    -gamepad1.right_stick_x
+            )
+    );
+}else {
+//the following setting will scale the input
+    drive.setWeightedDrivePower(
+            new Pose2d(
+                    input.getX(),
+                    input.getY(),
+                    -gamepad1.right_stick_x * 0.7
+            )
+    );
+}
+
+
 
             // Update everything. Odometry. Etc.
             drive.update();
